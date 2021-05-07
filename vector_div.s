000c776424d73073bd604758dbfc3b7ce9b5c3df
dillynewst <78389635+dillynewst@users.noreply.github.com>
2021-05-07 17:19:52 -0500

    Create vector_div.s

---
+++ vector_div.s
@@ -0,0 +1,59 @@
+.data
+n: .word 3
+vector1: .word 25,16,64,
+vector2: .word 5,2,8,
+vector3: .word 0,0,0,
+
+.text
+	la  s0, n
+    lw, s0, 0(s0) # s0 is n
+    la s1, vector1 #load vector1 into s1
+    la s2, vector2 #load vector2 into s2
+    la s3, vector3
+    mv t0, s1
+    mv t1, s2
+    mv t3, s3
+    li t2, 0
+    #for some reason only adds the first number in
+    #vector2 to the entirety of vector1
+    
+addLoop:
+	beq t2, s0, endLoop
+	lw a1, 0(t0)
+    lw a2, 0(t1)
+    div a1, a1, a2
+    li a0, 1
+    ecall
+    li a1, ','
+    li a2, ','
+    li a0, 11
+    addi t0, t0, 4
+    addi t1, t1, 4
+    addi t2, t2, 1
+    ecall
+    j addLoop
+    
+subLoop:
+	beq t2, s0, endLoop
+	lw a1, 0(t0)
+    lw a2, 0(t1)
+    div a1, a1, a2
+    li a0, 1
+    ecall
+    li a1, ','
+    li a2, ','
+    li a0, 11
+    addi t0, t0, 4
+    addi t2, t2, 1
+    ecall
+    j subLoop
+    
+endLoop:
+	li, a1, '\n'	#
+    li a0, 11		# printing a new line
+    ecall			#
+	mv a1, t1
+    li a0, 1
+    ecall
+    li a0, 10
+    ecall
